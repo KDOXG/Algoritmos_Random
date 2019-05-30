858ca0e8bb65e47b3c215f077c847e83572ab601
KDOXG <kadox0.g@gmail.com>
2019-05-29 23:13:03 -0300

    Correções

--- arvore_binaria_-_busca_em_largura.c
+++ arvore_binaria_-_busca_em_largura.c
@@ -1,52 +1,56 @@
 int FilaTemElementos(arvore **fila, int tam)
 {
-    int i, flag=1;
+    int i, flag=0;
     for (i=0; i<tam; i++)
     {
         if (fila[i] == NULL)
-        {
-            flag = 0;
-            break;
-        }
+            flag++;
     }
-    return flag;
+    if (flag == tam-1)
+        return 0;
+    else
+        return 1;
 }
 
-arvore* GetNodo(arvore *nodo)
+arvore* GetNodo(arvore *nodo, short flag)
 {
-    if (nodo == NULL)
+    if (!!nodo)
         return NULL;
+    if (flag)
+        return filaNodos[j]->pDir;
     else
-        return nodo;
+        return filaNodos[j]->pEsq;
 }
 
 void arvoreSimetrica(arvore *raiz, int quantidadeElementos)
 {
-    int *vetor, altura=2, filaTamanho=2, aux, i=0, j=0, k=1;
-    arvore **filaNodos, **filaNodos_aux;
-    filaNodos = malloc((altura)*sizeof(arvore*));
+    int altura=2, filaTamanho=2, aux, i=0, j=0, k=1;
+    arvore **vetor, **filaNodos, **filaNodos_aux;
+    filaNodos = malloc((filaTamanho)*sizeof(arvore*));
     filaNodos[0] = raiz->pEsq;
     filaNodos[1] = raiz->pDir;
-    vetor = malloc(quantidadeElementos*sizeof(int));
-    vetor[0] = raiz->dado; //Se o dado for um inteiro, senao so alterar o tipo do vetor
+    vetor = malloc(quantidadeElementos*sizeof(arvore*));
+    vetor[0] = raiz;
     for (i=0; i<filaTamanho; i++)
     {
         if (filaNodos[i] != NULL)
         {
-            vetor[k] = filaNodos_aux[i]->dado;
+            vetor[k] = filaNodos_aux[i];
             k++;
         }
     }
+    i=0;
 
-    for (filaTamanho=4; FilaVazia(filaNodos,filaTamanho); filaTamanho *= 2)
+    while(FilaTemElementos(filaNodos,filaTamanho))
     {
+        filaTamanho *= 2;
         filaNodos_aux = malloc(filaTamanho*sizeof(arvore*));
         aux = altura;
         while (aux > 0)
         {
-            filaNodos_aux[i] = GetNodo(filaNodos[j]->pEsq);
+            filaNodos_aux[i] = GetNodo(filaNodos[j],0);
             i++;
-            filaNodos_aux[i] = GetNodo(filaNodos[j]->pDir);
+            filaNodos_aux[i] = GetNodo(filaNodos[j],1);
             i++;
             j++;
             aux--;
@@ -55,7 +59,7 @@ void arvoreSimetrica(arvore *raiz, int quantidadeElementos)
         {
             if (filaNodos_aux[i] != NULL)
             {
-                vetor[k] = filaNodos_aux[i]->dado;
+                vetor[k] = filaNodos_aux[i];
                 k++;
             }
         }
